19c570b5bea76ade4633181dd83c232563131a4d
karasu yami <demigodlinty@gmail.com>
2022-12-07 16:05:39 -0600

    reverted to old concept seems to be the most working one

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,7 @@
-<<<<<<< HEAD
-const fs = require("fs");
-const  dirname  = require("path");
-const uuid = require('uuid')
-const router = require('express').Router();
-
-
-router.get('/notes',(req,res) =>{
- const data = fs.readFileSync('../db/db.json')
- res.json(JSON.parse(data)); 
-});
-router.post("/notes", (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('./db/db.json', {encoding:'utf16le'}));
-    const dataBody = req.body
-    dataBody.id = uuid.v4();
-    notes.push(dataBody)
-    fs.writeFileSync('../db/db.json', JSON.stringify(note));
-    res.json(notes)
-});
-=======
 const express = require('express')
 const app = express()
 const router = require('express').Router();
-const { readFromFile, readAndAppend } = require('../db/fsUtils');
+const { readFromFile, readAndAppend } = require('../tools/fsUtils');
 
 router.get('/notes',(req,res) =>{
   readFromFile('.db/noteData.json').then((data) => res.json(JSON.parse(data)))
@@ -45,6 +25,5 @@ router.get('/notes',(req,res) =>{
             default:
                 res.json('UH OH!: cant add new note :(')
     }
->>>>>>> parent of d446d28 (made index file connect to notes file)
-
-module.exports = router
+})
+module.exports = router;
--- routes/hypertext.js
+++ routes/hypertext.js
@@ -12,13 +12,6 @@ router.get('/notes', (req,res) => {
 })
 
 router.get('*', (req, res) =>{
-<<<<<<< HEAD
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-});
-
-
-module.exports = router
-=======
     res.sendFile(path.join(__dirname, '/public/index.html'))
     readfromFile('./db/noteData.json').then((data) =>
     res.json(JSON.parse(data))
@@ -27,4 +20,3 @@ module.exports = router
 
 
 module.exports = router
->>>>>>> parent of d446d28 (made index file connect to notes file)
