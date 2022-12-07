ad891a555a641751214a496b9bd7d7effa1332d5
karasu yami <demigodlinty@gmail.com>
2022-12-07 16:14:35 -0600

    added slight changes

--- routes/hypertext.js
+++ routes/hypertext.js
@@ -1,22 +1,22 @@
 
-const path = require('path')
+const path = require('path');
 const router = require('express').Router();
-const db = require('../db/db.json')
+
 
 router.get('/notes', (req,res) => {
-  res.sendFile (path.join(__dirname, '../public/index.html'))
+  res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 router.get('/notes', (req,res) => {
-  res.sendFile (path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '../public/notes.html'))
 })
 
 router.get('*', (req, res) =>{
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
     readfromFile('./db/noteData.json').then((data) =>
     res.json(JSON.parse(data))
     );
 })
 
 
-module.exports = router
+module.exports = router;
